519c84f15f461b19e18510e19f732ac20b278653
DANIEL AZULAY <daniel.a@cyolo.io>
2022-11-15 18:11:51 +0200

    first

---
+++ automation.sh
@@ -0,0 +1,31 @@
+# Install brew:
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+
+echo ""
+echo "Updating homebrew"
+brew update
+
+dev_apps=(
+	# iterm2
+  # docker
+	# google-chrome
+	# visual-studio-code
+  # goland
+  # spotify
+  slack
+)
+brew install ${dev_apps[@]} --cask
+
+echo ""
+echo "Installing ohmyzsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo ""
+echo "Installing Yarn"
+brew install yarn
+
+echo "Done!"
